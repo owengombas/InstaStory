2c67c28e0ceb33c61d8a8fa4b1d89537c4671726
Owen Calvin <owen.gombas@gmail.xom>
2019-11-26 08:25:24 +0100

    update(users list)

--- src/datas/users.ts
+++ src/datas/users.ts
@@ -1,8 +1,3 @@
 export const users = [
-    "steeleism", "mileyofficiaal", "galleryofmiley", "iwoulddieformiley", "mileyrcyrusdoll",
-    "tessamaethompson", "stylishcyrus_", "selmablair", "derekblasberg", "jbadvss", "unmiley", "alokvmenon",
-    "hq.mileycyrus", "deeegoldie", "thedodo", "tonicornell", "carlijnjacobs", "insidethemood", "javiercyruss", "tyrabanks",
-    "reesewitherspoon", "tomokoizumi", "valeriemorriscampbell", "gigihadid", "mileyxoj", "mikewillmadeit",
-    "everything.cyrus", "studiofredericsanchez", "damienhirst", "alexandrevauthier", "giizeleoliveira", "miumiu",
-    "miumiuwomenstales", "haileybentongates", "telfarglobal"
+    "steeleism"
 ];
